7d6dc02e6955ea68375920c9f1be211322737ca5
Jonathan Yu <yu021@csiro.au>
2019-07-11 14:46:12 +1000

    fix utils typo

--- pyloci/sparql/util.py
+++ pyloci/sparql/util.py
@@ -467,7 +467,7 @@ def query_mb16cc_contains(regionUri, sparql_endpoint, auth=None):
         res_list.append( { #?from ?pred ?to ?fromArea ?toArea ?toParent
                     'from': res['from']['value']  if 'from' in res else None, 
                     'to': res['to']['value']  if 'to' in res else None, 
-                    'fromAreaLinkSet': res['fromAreaLinkSet']['value'] if 'fromAreaLinkSet' in res else None, 
+                    'fromAreaLinkset': res['fromAreaLinkset']['value'] if 'fromAreaLinkset' in res else None, 
                     'fromAreaDataset': res['fromAreaDataset']['value'] if 'fromAreaDataset' in res else None, 
                     'toAreaLinkset' : res['toAreaLinkset']['value'] if 'toAreaLinkset' in res else None ,
                     'toAreaDataset' : res['toAreaDataset']['value'] if 'toAreaDataset' in res else None ,
