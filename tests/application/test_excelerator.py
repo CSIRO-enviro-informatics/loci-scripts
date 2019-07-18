9b84a7d6c716f776697ddc8b7cd7b1400c602fe0
Ben Leighton <Ben.Leighton@csiro.au>
2019-07-18 15:05:42 +1000

    travis will ignore application level tests, docker-compose for running application level tests application level tests uptime and, selenium based, page title some application tests may want to move back into excelerator code base

--- tests/application/test_excelerator.py
+++ tests/application/test_excelerator.py
@@ -1,5 +1,7 @@
 import pytest
 import requests
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
 import os
 
 def test_exelerator_200_response():
@@ -12,4 +14,14 @@ def test_exelerator_200_response():
 def test_exelerator_valid_page_contents():
     '''Test
     '''
-    assert(True)
+    chrome_options = webdriver.chrome.options.Options()
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--disable-gpu')
+    driver = webdriver.Chrome(options=chrome_options)
+    driver.set_page_load_timeout(60)
+    driver.implicitly_wait(10)
+    EXELERATOR_URL = os.getenv("EXCELERATOR_URL")
+    driver.get(EXELERATOR_URL)
+    assert(driver.title == 'Excelerator')
+    #assert(len(found_elements) > 0)
