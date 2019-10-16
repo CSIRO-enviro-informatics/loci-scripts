4b6b4a153f130790fa14a49992d95a0aff91f3a5
jyucsiro <jonathan.yu@csiro.au>
2019-10-16 17:45:38 +1100

    update list of loci feature types

--- pyloci/sparql/generate_loci_type_count.py
+++ pyloci/sparql/generate_loci_type_count.py
@@ -21,23 +21,30 @@ if(GRAPHDB_USER != None and GRAPHDB_PASSWORD != None):
     }
 
 loci_types = [
-    "<http://linked.data.gov.au/def/geofabric#ContractedCatchment>",
     "geo:Feature",
     "hyf:HY_Catchment",
     "hyf:HY_HydroFeature",
-    "<http://linked.data.gov.au/def/geofabric#RiverRegion>",
     "loci:Linkset",
-    "<http://linked.data.gov.au/def/asgs#MeshBlock>",
     "geo:Geometry",
     "<http://linked.data.gov.au/def/asgs#DestinationZone>",
     "<http://linked.data.gov.au/def/asgs#NaturalResourceManagementRegion>",
     "<http://linked.data.gov.au/def/asgs#StateSuburb>",
+    "<http://linked.data.gov.au/def/asgs#GreaterCapitalCityStatisticalArea>",
+    "<http://linked.data.gov.au/def/geofabric#ContractedCatchment>",
+    "<http://linked.data.gov.au/def/geofabric#DrainageDivision>",
+    "<http://linked.data.gov.au/def/geofabric#RiverRegion>",
+    "<http://linked.data.gov.au/def/gnaf#Address>",
+    "<http://linked.data.gov.au/def/gnaf#Locality>",
+    "<http://linked.data.gov.au/def/geofabric#ReportingRegion>",
+    "<http://linked.data.gov.au/def/gnaf#Street>",
+    "<http://linked.data.gov.au/def/loci#Feature>",
+    "<http://linked.data.gov.au/def/gnaf#StreetLocality>",
+    "<http://linked.data.gov.au/def/asgs#MeshBlock>",
     "<http://linked.data.gov.au/def/asgs#StateOrTerritory>",
     "<http://linked.data.gov.au/def/asgs#StatisticalAreaLevel1>",
     "<http://linked.data.gov.au/def/asgs#StatisticalAreaLevel2>",
     "<http://linked.data.gov.au/def/asgs#StatisticalAreaLevel3>",
-    "<http://linked.data.gov.au/def/asgs#StatisticalAreaLevel4>",
-    "<http://linked.data.gov.au/def/asgs#GreaterCapitalCityStatisticalArea>"
+    "<http://linked.data.gov.au/def/asgs#StatisticalAreaLevel4>"
 ]
 
 #query()
