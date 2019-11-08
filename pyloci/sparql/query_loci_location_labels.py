2337d6f6443d76147a83f8b5013c15bff351261e
jyucsiro <jonathan.yu@csiro.au>
2019-11-08 12:56:29 +1100

    updating script and code for getting location labels

--- pyloci/sparql/query_loci_location_labels.py
+++ pyloci/sparql/query_loci_location_labels.py
@@ -2,13 +2,22 @@ from SPARQLWrapper import SPARQLWrapper, JSON
 import json 
 import os
 from dotenv import load_dotenv, find_dotenv
+import argparse
 load_dotenv(find_dotenv())
+
 
 from . import util
 
 GRAPHDB_USER = os.getenv("GRAPHDB_USER")
 GRAPHDB_PASSWORD = os.getenv("GRAPHDB_PASSWORD")
 SPARQL_ENDPOINT =  os.getenv("SPARQL_ENDPOINT")
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--offset", help="offset", type=int, default=0)
+parser.add_argument("--limit", help="limit", type=int, default=100000)
+parser.add_argument("--max", help="max", type=int, default=None)
+args = parser.parse_args()
+
 
 # uncomment the following GRAPHDB_SPARQL and auth variables for test repo
 #GRAPHDB_SPARQL = GRAPHDB_SPARQL_TEST
@@ -20,5 +29,5 @@ if(GRAPHDB_USER != None and GRAPHDB_PASSWORD != None):
         "password" : GRAPHDB_PASSWORD   
     }
 
-labels = util.query_labels_from_locations(SPARQL_ENDPOINT, auth=auth)
+labels = util.query_labels_from_locations(SPARQL_ENDPOINT, auth=auth, offset=args.offset, limit=args.limit, max=args.max)
 
