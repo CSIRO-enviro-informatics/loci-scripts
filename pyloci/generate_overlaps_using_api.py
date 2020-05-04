7c328c2543f36e7ee00485e726aa018b8485f0da
jyucsiro <jonathan.yu@csiro.au>
2020-05-04 22:32:42 +1000

    add outputfile parameter on generate overlaps

--- pyloci/generate_overlaps_using_api.py
+++ pyloci/generate_overlaps_using_api.py
@@ -33,7 +33,7 @@ def stringify_uri_to_filename(uri):
 
 
 
-def query_target_features(source_feature_uri, target_feature_type_shortlabel, target_dataset_uri):
+def query_target_features(source_feature_uri, target_feature_type_shortlabel, target_dataset_uri, output_filename=None):
     '''
     Returns a list of target feature types for source feature uri
     '''
@@ -51,7 +51,10 @@ def query_target_features(source_feature_uri, target_feature_type_shortlabel, ta
 
     
     #write this to file
-    filename = "overlaps_to_{}_{}.json".format(target_feature_type_shortlabel, stringify_uri_to_filename(source_feature_uri))
+    if(output_filename == None):
+        filename = "overlaps_to_{}_{}.json".format(target_feature_type_shortlabel, stringify_uri_to_filename(source_feature_uri))
+    else:
+        filename = output_filename
     with open(filename, 'w') as outfile:
         print("Writing to: " + filename)
         json.dump(list_locations, outfile, indent=4)
@@ -59,12 +62,13 @@ def query_target_features(source_feature_uri, target_feature_type_shortlabel, ta
     return 
 
 
-def main(source_feature_type_shortlabel, target_feature_type_shortlabel, source_dataset=None, target_dataset_uri=None, num_source_features=1000000, single_source_uri=None):
+def main(source_feature_type_shortlabel, target_feature_type_shortlabel, source_dataset=None, target_dataset_uri=None, 
+            num_source_features=1000000, single_source_uri=None, output_filename=None):
     target_dict = {}
 
     if single_source_uri != None:
         print("Querying source {}...".format(single_source_uri))
-        query_target_features(single_source_uri, target_feature_type_shortlabel, target_dataset_uri)
+        query_target_features(single_source_uri, target_feature_type_shortlabel, target_dataset_uri, output_filename)
         return
 
     source_list = query_list_of_source_feature_type(source_feature_type_shortlabel, max=num_source_features)
@@ -74,7 +78,7 @@ def main(source_feature_type_shortlabel, target_feature_type_shortlabel, source_
         i = i + 1
         #store counts for now    
         print("Querying source {} of {}...".format(i, num_sources))
-        query_target_features(s, target_feature_type_shortlabel, target_dataset_uri)
+        query_target_features(s, target_feature_type_shortlabel, target_dataset_uri, output_filename)
     return
         
 import sys, argparse
@@ -85,6 +89,7 @@ if __name__ == "__main__":
     parser.add_argument('--source-dataset-uri', dest='source_dataset', help='Source dataset URI')
     parser.add_argument('--single-source-instance-uri', dest='single_source_uri', help='Run for a single source dataset instance URI')
     parser.add_argument('--target-dataset-uri', dest='target_dataset',help='Target dataset URI')
+    parser.add_argument('--output', dest='output_filename',help='Target output file')
     parser.add_argument('--num-source-features', dest='num_source_features',help='Max number of source features')
 
 
@@ -101,5 +106,8 @@ if __name__ == "__main__":
     single_source_uri = None
     if args.single_source_uri:
         single_source_uri = args.single_source_uri
+    output_filename = None
+    if args.output_filename:
+        output_filename = args.output_filename
 
-    main(args.source_feature_type_shortlabel, args.target_feature_type_shortlabel, source_dataset, target_dataset, num_source_features, single_source_uri)
+    main(args.source_feature_type_shortlabel, args.target_feature_type_shortlabel, source_dataset, target_dataset, num_source_features, single_source_uri, output_filename)
