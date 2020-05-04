7102a2d7bdbd096ea763546bfc81bde93c710dbb
jyucsiro <jonathan.yu@csiro.au>
2020-05-04 22:32:27 +1000

    add crosswalk and within walk tests

---
+++ generate_overlaps.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+#python -m pyloci.generate_overlaps_using_api lga sa1 \
+#    --target-dataset-uri http://linked.data.gov.au/dataset/asgs2016 \
+#    --output Test-D-W-1 \
+#    --single-source-instance-uri http://linked.data.gov.au/dataset/asgs2016/localgovernmentarea/55880 
+
+#python -m pyloci.generate_overlaps_using_api sa1 lga \
+#    --target-dataset-uri http://linked.data.gov.au/dataset/asgs2016 \
+#    --output Test-D-W-2 \
+#    --single-source-instance-uri http://linked.data.gov.au/dataset/asgs2016/statisticalarealevel1/80105105310 
+
+#python -m pyloci.generate_overlaps_using_api ra sa2 \
+#    --target-dataset-uri http://linked.data.gov.au/dataset/asgs2016 \
+#    --output Test-D-W-3 \
+#    --single-source-instance-uri http://linked.data.gov.au/dataset/asgs2016/remotenessarea/44
+
+#python -m pyloci.generate_overlaps_using_api ra sa1 \
+#    --target-dataset-uri http://linked.data.gov.au/dataset/asgs2016 \
+#    --output Test-D-W-4 \
+#    --single-source-instance-uri http://linked.data.gov.au/dataset/asgs2016/remotenessarea/44
+
+#python -m pyloci.generate_overlaps_using_api ra mb \
+#    --target-dataset-uri http://linked.data.gov.au/dataset/asgs2016 \
+#    --output Test-D-W-5 \
+#    --single-source-instance-uri http://linked.data.gov.au/dataset/asgs2016/remotenessarea/44
+
+#python -m pyloci.generate_overlaps_using_api ra lga \
+#    --target-dataset-uri http://linked.data.gov.au/dataset/asgs2016 \
+#    --output Test-D-W-6 \
+#    --single-source-instance-uri http://linked.data.gov.au/dataset/asgs2016/remotenessarea/44
+
+
+#python -m pyloci.generate_overlaps_using_api ra cc \
+#    --output Test-D-X-2 \
+#    --single-source-instance-uri http://linked.data.gov.au/dataset/asgs2016/remotenessarea/44
+
+#python -m pyloci.generate_overlaps_using_api lga cc \
+#    --output Test-D-X-3 \
+#    --single-source-instance-uri http://linked.data.gov.au/dataset/asgs2016/localgovernmentarea/55880
+
+
+#python -m pyloci.generate_overlaps_using_api lga rr \
+#    --output Test-D-X-4 \
+#    --single-source-instance-uri http://linked.data.gov.au/dataset/asgs2016/localgovernmentarea/55880
+
+#python -m pyloci.generate_overlaps_using_api lga dd \
+#    --output Test-D-X-5 \
+#    --single-source-instance-uri http://linked.data.gov.au/dataset/asgs2016/localgovernmentarea/55880
+
+
+#python -m pyloci.generate_overlaps_using_api iloc sa1 \
+#    --target-dataset-uri http://linked.data.gov.au/dataset/asgs2016 \
+#    --output Test-D-W-3 \
+#    --single-source-instance-uri http://linked.data.gov.au/dataset/asgs2016/indigenouslocation/10600402 
+
+
+#python -m pyloci.generate_overlaps_using_api sua sa1 \
+#    --target-dataset-uri http://linked.data.gov.au/dataset/asgs2016 \
+#    --output Test-D-W-4 \
+#    --single-source-instance-uri http://linked.data.gov.au/dataset/asgs2016/significanturbanarea/1000 
+
+
---
+++ tests/integration/test_loci_api_topology-D-same-dataset-walk.py
@@ -0,0 +1,101 @@
+import pytest
+from SPARQLWrapper import SPARQLWrapper, JSON
+import os
+import csv
+import json
+from dotenv import load_dotenv, find_dotenv
+load_dotenv(find_dotenv())
+from pyloci.api import util
+
+def get_csvdata(fname, delim='\t'):
+    with open(fname, 'r', newline='') as csv_file:
+        return list(csv.DictReader(csv_file, delimiter=delim))
+
+def id_list(data):
+    return [i['testcase'] for i in data]
+
+def input_list(data):
+    return [i['input'] for i in data]
+
+def expected(data):
+    return [i['input'] for i in data]
+
+
+inputdata = get_csvdata('./loci-testdata/test-case-d/input/loci-test-case-D-withindataset.csv', delim="\t")
+
+api_util = util.Util()
+LOCI_INTEGRATION_API = os.getenv("LOCI_INTEGRATION_API")
+
+def run_query(uri, loci_type):
+    res  = api_util.query_api_location_overlaps(uri, loci_type, LOCI_INTEGRATION_API, crosswalk=True)
+    return res
+
+
+@pytest.mark.parametrize('testinput', inputdata, ids=id_list(inputdata))
+def test_loci_topology(testinput):
+    '''Test 
+    '''
+    print(testinput)
+    #query cache back on the input data
+    #print(testinput)
+    #with open('./loci-testdata/test-case-d/Test-' + testinput['testcase'] + '.json') as json_file:
+
+    inputObj = testinput['input']
+    #print(inputObj)
+    t =  testinput['target_region_type'] 
+
+    featureTypeShortName = inputObj[0]
+
+    #print(p)
+    #print(t)
+    print(inputObj)
+    if t != None and inputObj != None:
+        test_instance_uri = inputObj 
+    
+        
+        with open('./loci-testdata/test-case-d/Test-' + testinput['testcase'] + '.json') as json_file:
+            expecteddata = json.load(json_file)
+            print(test_instance_uri)
+            #print(expecteddata)
+        
+            #run sparql query to get the sfContains of the test_instance_uri obj
+            matches = run_query(test_instance_uri, t)
+            #turn list of dict to plain list
+            #print(matches)
+            if "message" in matches:
+                assert matches['code'] != 500
+
+            list_of_matching_uris = list(map(lambda x : x['uri'], matches['overlaps']))
+            list_of_expecting_uris = list(map(lambda x : x['uri'], expecteddata['overlaps']))
+
+            #print(list_of_matching_uris)
+            print(matches)
+            assert match_expected_data_with_matches(testinput['testcase'], list_of_expecting_uris, list_of_matching_uris, t)
+    else:
+        assert False
+
+def check_expected_data_in_matches(testcase_id, expected_list, matches_list, featureTypeShortName):
+    # things in matches not in expected
+    #print(expected_with_angle_brackets_list)
+    diff = sorted(list(set(matches_list) - set(expected_list)))
+    # things in expected not in matches
+    diff2 = sorted(list(set(expected_list) - set(matches_list)))
+    print(len(expected_list))
+    print(len(matches_list))
+    print(len(diff2))
+    if(len(diff2) < len(matches_list)):
+        return True
+    return False
+
+def match_expected_data_with_matches(testcase_id, expected_list, matches_list, featureTypeShortName):
+    # things in matches not in expected
+    #print(expected_with_angle_brackets_list)
+    diff = sorted(list(set(matches_list) - set(expected_list)))
+    # things in expected not in matches
+    diff2 = sorted(list(set(expected_list) - set(matches_list)))
+    print(diff2)
+    print(len(diff2))
+    if(len(diff2) == 0):
+        return True
+    return False
+
